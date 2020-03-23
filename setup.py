a359438cd7ee6f75129a096e5b43700c063925b7
Izainea <cizaineam@gmail.com>
2020-03-23 18:16:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'UNATD',
+  packages = ['UNATD'], # this must be the same as the name above
+  version = '0.1',
+  description = 'paquete de aprendizaje de TDA - UNAL Colombia',
+  author = 'Isaac Zainea',
+  author_email = 'isaaczainea@gmail.com',
+  url = 'https://github.com/izainea/UNATD', # use the URL to the github repo
+  download_url = 'https://github.com/izainea/UNATD/tarball/0.1',
+  keywords = ['testing', 'ATD', 'TDA'],
+  classifiers = [],
+)
